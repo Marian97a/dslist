bbfca9d88f3cd404db3e4ce4c16650538e294d16
Maria Viana do Nascimento <mariaviana05091997@gmail.com>
2024-11-14 15:15:39 -0300

    Consultas SQL, Games por lista

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 
@@ -31,4 +32,10 @@ public class GameService {
 		return result.stream().map(x-> new GameMinDTO(x)).toList();
 		
 	}
+    @Transactional(readOnly = true)
+   	public List<GameMinDTO> findByList(Long listId){
+    	List<GameMinProjection> result = gameRepository.searchByList(listId);
+   		return result.stream().map(x-> new GameMinDTO(x)).toList();
+   		
+   	}
 }
